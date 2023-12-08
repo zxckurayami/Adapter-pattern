e91f91286ff6a681ddeb124606189ff4b226d3a3
zxckurayami <zxcshadowfiend2006@gmail.com>
2023-12-08 12:00:52 +0200

    Добавьте файлы проекта.

---
+++ ConsoleAppUA1/ConsoleApplication1.cpp
@@ -0,0 +1,41 @@
+﻿// Виконав Іван Єнко
+#include <Windows.h>
+#include <iostream>
+
+// Старий інтерфейс
+class OldLibrary {
+public:
+    void oldMethod() {
+        std::cout << "Old Library Method" << std::endl;
+    }
+};
+
+// Новий інтерфейс, який ми хочемо використовувати
+class NewInterface {
+public:
+    virtual void newMethod() = 0;
+};
+
+// Адаптер, який забезпечує взаємодію нового інтерфейсу зі старим
+class Adapter : public NewInterface {
+private:
+    OldLibrary oldLibrary;
+
+public:
+    void newMethod() override {
+        // Викликаємо старий метод через адаптер
+        oldLibrary.oldMethod();
+    }
+};
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    // Створення об'єкта адаптера
+    Adapter adapter;
+
+    // Використання нового інтерфейсу через адаптер
+    adapter.newMethod();
+
+    return 0;
+}
